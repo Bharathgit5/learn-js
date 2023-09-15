3e7533f80a196b8460ac0058c00f7a7359cb0563
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2023-09-15 17:27:27 +0000

    js-basics-01

---
+++ basics_01/datatypes_02.js
@@ -0,0 +1,38 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) //get output in browser but we are using nodejs
+
+console.log(3
+    +
+    3);  console.log("bharath");//output comes but code is not readable
+
+//data types
+let age=19  //no type like int, float etc..
+
+let name="bharath"
+//boolean
+
+let isLoggedin=true
+
+let test = 8>6 
+console.table([age,name,isLoggedin,test]);
+
+//arrays
+let array = ['bharath','shivam','kiran']
+console.log(array[0])
+
+//object
+let bharath={
+    age:12,
+    branch:'ECM',
+    subject:'OOPS THROUGH JAVA',
+    marks:9.0
+}
+console.log(bharath.subject)
+
+//null - intentional absence
+let knowledge=null  //object
+console.log(knowledge)
+
+let tell  //undefined
+console.log(tell)
---
+++ basics_01/variables_01.js
@@ -0,0 +1,24 @@
+//LOOK AT COMMENTS WHILE REVISION FOR EASY UNDERSTANDING
+
+const accountId = 21671
+
+let accountEmail = "bharath@gmail.com"
+
+var accountcity="hyderabad"
+
+/*
+myName = "Chris";
+
+function logName() {
+  console.log(myName);
+}
+
+logName();
+
+var myName;
+var keyword is outdated only use const-which doesnot change let-can change values
+because of issue in block scope and functional scope
+*/
+// accountId = 192313  //error
+accountEmail="my@gmail.com"
+console.table([accountId, accountEmail,accountcity]) //console.table
