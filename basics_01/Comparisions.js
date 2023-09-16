b20030603b624dd56cef9fbec80a614e366489d2
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2023-09-16 12:55:55 +0000

    comparisions-04

---
+++ basics_01/Comparisions.js
@@ -0,0 +1,28 @@
+//Comparisions of datatypes
+/*console.log(2>1);
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1) 
+*/
+/*console.log("2">1);
+console.log("02">1)*/
+
+//null = 0
+/*eqaulity check == and comparison > < >= <= work differently.
+comparisons convert null to a number, trating it as 0.
+thats why (3) null >=0 is true and (1) null >0 is false*/
+/*console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+*/
+//undefined gives always false
+/*
+console.log(undefined ==0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+*/
+
+/*console.log("22"==22) loose equality like OR
+console.log("22"===22) strict equality like AND */ 
+// === checks datatypes also
