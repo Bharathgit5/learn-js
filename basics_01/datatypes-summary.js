ce4eb7a9939eaa287e9f1353197acc25ce4be29e
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2023-09-16 13:53:51 +0000

    datatypes-summary

---
+++ basics_01/datatypes-summary.js
@@ -0,0 +1,48 @@
+/* data types in js are of two types 
+1.primitive datatype
+2.non primitive datatype
+
+1.primitive datatype
+String - let color = "black"
+         let name = "bharath"
+         console.log(color)
+
+Number - let length=34
+         let weight = 59
+         console.log(weight)
+
+Bigint - let bignum = BigInt("232123232221212122323232")
+         console.log(bignum)
+
+Boolean - let x=5
+          let y =5;
+          let z =9;
+          (x==y)
+          (x==z)
+
+
+Undefined - let userEmail;
+            console.log(userEmail)
+
+Null - const outsideTemp=null
+       console.log(outsideTemp)
+
+Symbol - UNIQUE 
+         const id = Symbol('123')
+         const id2 = Symbol('123')
+         console.log(id == id2)
+
+ */
+/*Non primitive (reference) 
+Array,Objects,Functions
+Arrays - const heros = ["pawankalyan","virat","steeve"];
+Object - let mydetails = {
+    name:'bharath',
+    age:98,
+    phone:9879878980
+}
+Functions - const myFunction = function(){
+    console.log("bharath")
+}
+ console.log(typeof(mydetails))
+*/
