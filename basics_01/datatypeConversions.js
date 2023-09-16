f65d523cb0e136069a87815b5387a802dc657a52
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2023-09-16 01:43:37 +0000

    datatype-conversions

---
+++ basics_01/datatypeConversions.js
@@ -0,0 +1,27 @@
+let score = "23kjk"
+
+//console.log(typeof(score))
+//console.log(score)
+
+//let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+// "23" => 23
+//"33waf" => NaN
+//true = 1; false = 0;
+
+let isLoggedIn = ""
+
+let isLoggedInBoolean = Boolean(isLoggedIn)
+//console.log(isLoggedInBoolean)
+//1 => true; 0 => false
+//"" => false
+//"bharath" => true
+
+let num = 23;
+let stringnum = String(num)
+console.log(stringnum)
+console.log(typeof(stringnum))
+let sum = stringnum
+console.log(sum)
