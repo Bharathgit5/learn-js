dea96491901ecdc7b33d036a8d1ba046946dc347
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2023-09-18 09:13:26 +0000

    array-02

---
+++ basics_02/02_arrays.js
@@ -0,0 +1,8 @@
+// +++++++++++++ Methods of arrays ++++++++++++++++
+// .of .from .Array
+const arr = [1,2,3,4,5]
+const arr1 = [6,7,8,9,10]
+const arr2 = [11,12,13,14]
+console.log(Array.isArray(arr1)) // is array or not
+console.log(Array.from('bharath')) // makes into array
+// learn more about .of 
