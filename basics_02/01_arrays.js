3663e9391569fb6dc519b8019673bc68da5ac0d9
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2023-09-18 09:00:15 +0000

    arrays_01

--- basics_02/01_arrays.js
+++ basics_02/01_arrays.js
@@ -37,6 +37,10 @@ console.log(names.unshift('keerthi')) //adds new element in first
 const rollno =[12,23,34,4]
 console.log(names.concat(rollno))
 
+// another way of concat
+const studentdetails = [...names,...rollno]
+console.log(studentdetails)
+
 //slice and splice
 const arr1 = [0,1,2,3,4,5]
 const newarr1 = arr1.slice(0,3) //removes other elements which are no included in (0-3) 3 is excluded i.e(0-2) will have and rest will be out
@@ -46,3 +50,6 @@ console.log(arr1) // main array arr1 will not change only newarr1 will change to
 const newarr2 = arr1.splice(0,3)// removes the elements which are mentioned in splice(0-3) 3 is excluded i.e (0-2) will be removed and [3,4,5] will have
 console.log(newarr2)
 console.log(arr1) // but here main array will change 
+
+const mainarr = [1,2,3,2,[2,1,3],21,4,5,[23,34,[23,34,[1]]]]
+console.log(mainarr.flat(Infinity))
