45f478b2367a70060f4b1aa00f4d0189db519702
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2023-09-20 17:03:56 +0000

    function with obj and arrays

--- basics_03/01_functions.js
+++ basics_03/01_functions.js
@@ -33,4 +33,89 @@ if(!username){
 }
     return `${username} just logged in`
 }
-console.log(loginUserMessage()) //if you not defined any argument here then you get undefined
+//console.log(loginUserMessage()) //if you not defined any argument here then you get undefined
+
+//functions with objects and arrays
+function calculateCartPrice(...num1){ // to store multiple values in one parameter use ...num1
+
+    return num1
+}
+//console.log(calculateCartPrice(99,199,299))
+
+const user = { //predefine obj
+    username:'bharath',
+    price:199
+}
+
+//accessing objects using functions
+function hanldeObj(user){
+//    console.log(`username is ${user.username} and price is ${user.price}`)
+}
+//hanldeObj(user) 
+hanldeObj({ //post define obj
+    username:'hitesh',
+    price:121
+})
+
+//functions with arrays
+const arr = [1,2,3,4,5] //predefine
+
+function funcarr(arr){
+    return arr
+}
+//console.log(funcarr(arr))
+
+//console.log(funcarr([12,21,23,32]))
+
+ //my example and understanding of obj and functions
+
+ const ECM  = {
+    student1 :{
+        name:'bharath',
+        rollno:17,
+        result:true
+    },
+    student2 :{
+        name:'rahul',
+        rollno:10,
+        result:true
+    },
+    student3 :{
+        name:'ashok',
+        rollno:20,
+        result:true
+    },
+    student4 :{
+        name:'hasan',
+        rollno:11,
+        result:false
+    },
+    student5 :{
+        name:'huvi',
+        rollno:19,
+        result:false
+    }
+  
+ }
+
+ function ECMRESULTS(ECM){
+    console.log(`Dear Parents! Your ward ${ECM.student1.name} , RollNo : ${ECM.student1.rollno} . ${ECM.student1.result} in exam.`)
+ }
+
+ ECMRESULTS(ECM)
+
+ //my example and understanding of arrays and obj
+
+ const marks = [21,23,34,12,22]
+
+ function yourmarks(marks){
+    if(marks[3]<20){
+      console.log('you are failed')
+    }
+    else{
+        console.log("you are passed")
+    }
+ }
+
+ yourmarks(marks)
+ 
